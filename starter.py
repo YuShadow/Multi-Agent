26b868905e390e8cef6e432547f92676b6728d38
YuShadow <knightofstone6@gmail.com>
2026-08-09 02:30:37 +0300

    Delete starter.py

--- starter.py
+++
@@ -1,5 +0,0 @@
-#streamlit run main.py &> log.txt & <---- run in terminal first
-from pyngrok import ngrok
-ngrok.set_auth_token("3HGkO6mPOrf8wMeYwz8aUD1g3AG_5G9BNL6UWRCXU8ZL7yu6Y")
-public_url = ngrok.connect(8501)
-print(public_url)
